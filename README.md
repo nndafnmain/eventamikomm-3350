df530782ee40c9a9f15e9dec5e886379265e9df6
Ahmad Firza Ananda <nndafnmain@gmail.com>
2026-04-17 11:37:56 +0700

    Tugas 2

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-#IDENTITAS DIRI
+# IDENTITAS DIRI
+
 Nama: Ahmad Firza Ananda
 NIM: 24.12.3350
 Kelas: SI05
 
-#LINK APLIKASI:
+# LINK APLIKASI:
+
 https://eventamikomm-3350-main-ynwvsu.free.laravel.cloud/
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,44 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
+Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
-Route::get('/tentang', function() {
-    return '<h1>Ini adalah halaman tentang aplikasi Event Hub</h1>';
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/kontak', function() {
-    return view('contact');
+Route::get('/events', [EventController::class, 'indexAdmin'])->name('events.index');
+// dan seterusnya...
 });
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/profile', function(){
-    return view('profile');
-});
+// Route::get('/tentang', function() {
+//     return '<h1>Ini adalah halaman tentang aplikasi Event Hub</h1>';
+// });
 
-Route::get('/katalog', function(){
-    return view('katalog');
-});
+// Route::get('/kontak', function() {
+//     return view('contact');
+// });
 
-Route::get('/bantuan', function(){
-    return view('bantuan');
-});
+// Route::get('/profile', function(){
+//     return view('profile');
+// });
 
+// Route::get('/katalog', function(){
+//     return view('katalog');
+// });
+
+// Route::get('/bantuan', function(){
+//     return view('bantuan');
+// });
+
+// Route::get('/',)
 
